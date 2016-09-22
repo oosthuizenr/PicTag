388271e8b1dde08151eaccbd021535321c05c2c7
Renier Oosthuizen <oosthuizen.renier@gmail.com>
2016-09-22 20:49:05 +0200

    more work

--- app/src/main/java/com/beansontoast/pictag/interactor/MediaStoreInteractor.java
+++ app/src/main/java/com/beansontoast/pictag/interactor/MediaStoreInteractor.java
@@ -9,6 +9,8 @@ import android.provider.MediaStore;
 import com.beansontoast.pictag.model.MediaStoreData;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import rx.Observable;
@@ -20,43 +22,90 @@ import rx.Observable;
 public class MediaStoreInteractor {
     private Context mContext;
 
+    private String[] IMAGE_PROJECTION = new String[] {
+            MediaStore.Images.ImageColumns._ID,
+            MediaStore.Images.ImageColumns.DATE_TAKEN,
+            MediaStore.Images.ImageColumns.DATE_MODIFIED,
+            MediaStore.Images.ImageColumns.MIME_TYPE,
+            MediaStore.Images.ImageColumns.ORIENTATION,
+    };
+
+    private String[] VIDEO_PROJECTION = new String[] {
+            MediaStore.Video.VideoColumns._ID,
+            MediaStore.Video.VideoColumns.DATE_TAKEN,
+            MediaStore.Video.VideoColumns.DATE_MODIFIED,
+            MediaStore.Video.VideoColumns.MIME_TYPE,
+            "0 AS " + MediaStore.Images.ImageColumns.ORIENTATION,
+    };
+
     public MediaStoreInteractor(Context mContext) {
         this.mContext = mContext;
     }
 
     public Observable<List<MediaStoreData>> getMediaStoreData() {
         return Observable.defer(() -> Observable.create(subscriber -> {
-            List<MediaStoreData> toReturn = new ArrayList<>();
+            List<MediaStoreData> toReturn = queryImages();
+            toReturn.addAll(queryVideos());
 
-            String[] PROJECTION = new String[]{
-                    MediaStore.Images.ImageColumns._ID,
-                    MediaStore.Images.Media.DATA,
-                    MediaStore.Images.ImageColumns.MIME_TYPE,
-                    MediaStore.Images.ImageColumns.DATE_TAKEN};
-            Cursor cur = mContext.getContentResolver().query(
-                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                    PROJECTION,
-                    MediaStore.Images.ImageColumns.MIME_TYPE + " = 'image/jpeg'",
-                    null,
-                    null);
-
-            if (cur != null && cur.getCount() > 0) {
-                cur.moveToFirst();
-
-                do {
-                    long id = cur.getLong(cur.getColumnIndex(MediaStore.Images.ImageColumns._ID));
-                    long dateTaken = cur.getLong(cur.getColumnIndex(MediaStore.Images.ImageColumns.DATE_TAKEN));
-                    String data = cur.getString(cur.getColumnIndex(MediaStore.Images.ImageColumns.DATA));
-                    String mimeType = cur.getString(cur.getColumnIndex(MediaStore.Images.ImageColumns.MIME_TYPE));
-
-                    toReturn.add(MediaStoreData.getInstance(id, dateTaken, Uri.parse("file://" + data), mimeType));
-
-                } while (cur.moveToNext());
-            }
+            Collections.sort(toReturn, (mediaStoreData, mediaStoreData2) ->
+                    Long.valueOf(mediaStoreData2.getDateTaken()).compareTo(mediaStoreData.getDateTaken()));
 
             subscriber.onNext(toReturn);
             subscriber.onCompleted();
         }));
+    }
+
+    private List<MediaStoreData> queryImages() {
+        return query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, IMAGE_PROJECTION,
+                MediaStore.Images.ImageColumns.DATE_TAKEN, MediaStore.Images.ImageColumns._ID,
+                MediaStore.Images.ImageColumns.DATE_TAKEN, MediaStore.Images.ImageColumns.DATE_MODIFIED,
+                MediaStore.Images.ImageColumns.MIME_TYPE, MediaStore.Images.ImageColumns.ORIENTATION,
+                MediaStoreData.Type.Image);
+    }
+
+    private List<MediaStoreData> queryVideos() {
+        return query(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, VIDEO_PROJECTION,
+                MediaStore.Video.VideoColumns.DATE_TAKEN, MediaStore.Video.VideoColumns._ID,
+                MediaStore.Video.VideoColumns.DATE_TAKEN, MediaStore.Video.VideoColumns.DATE_MODIFIED,
+                MediaStore.Video.VideoColumns.MIME_TYPE, MediaStore.Images.ImageColumns.ORIENTATION,
+                MediaStoreData.Type.Video);
+    }
+
+    private List<MediaStoreData> query(Uri contentUri, String[] projection, String sortByCol,
+                                       String idCol, String dateTakenCol, String dateModifiedCol,
+                                       String mimeTypeCol, String orientationCol, MediaStoreData.Type type) {
+        List<MediaStoreData> toReturn = new ArrayList<>();
+
+        Cursor cursor = mContext.getContentResolver()
+                .query(contentUri, projection, null, null, sortByCol + " DESC");
+
+        if (cursor != null) {
+            try {
+                cursor.moveToFirst();
+
+                final int idColNum = cursor.getColumnIndexOrThrow(idCol);
+                final int dateTakenColNum = cursor.getColumnIndexOrThrow(dateTakenCol);
+                final int dateModifiedColNum = cursor.getColumnIndexOrThrow(dateModifiedCol);
+                final int mimeTypeColNum = cursor.getColumnIndex(mimeTypeCol);
+                final int orientationColNum = cursor.getColumnIndexOrThrow(orientationCol);
+
+                while (cursor.moveToNext()) {
+                    long id = cursor.getLong(idColNum);
+                    long dateTaken = cursor.getLong(dateTakenColNum);
+                    String mimeType = cursor.getString(mimeTypeColNum);
+                    long dateModified = cursor.getLong(dateModifiedColNum);
+                    int orientation = cursor.getInt(orientationColNum);
+
+                    toReturn.add(MediaStoreData.getInstance(id, dateTaken, dateModified,
+                            Uri.withAppendedPath(contentUri, Long.toString(id)),
+                            mimeType, orientation, type));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+
+        return toReturn;
     }
 
 }
--- app/src/main/java/com/beansontoast/pictag/model/MediaStoreData.java
+++ app/src/main/java/com/beansontoast/pictag/model/MediaStoreData.java
@@ -9,20 +9,32 @@ import android.provider.MediaStore;
 
 public class MediaStoreData {
 
-    private long mId;
-    private long mDateTaken;
-    private Uri mUri;
-    private String mMimeType;
-
-    private MediaStoreData(long mId, long mDateTaken, Uri mUri, String mMimeType) {
-        this.mId = mId;
-        this.mDateTaken = mDateTaken;
-        this.mUri = mUri;
-        this.mMimeType = mMimeType;
+    public enum Type {
+        Image,
+        Video
     }
 
-    public static MediaStoreData getInstance(long mId, long mDateTaken, Uri mUri, String mMimeType) {
-        return new MediaStoreData(mId, mDateTaken, mUri, mMimeType);
+    private long mId;
+    private long mDateTaken;
+    private long mDateModified;
+    private Uri mUri;
+    private String mMimeType;
+    private int mOrientation;
+    private Type mType;
+
+    private MediaStoreData(long mId, long mDateTaken, long mDateModified, Uri mUri, String mMimeType, int mOrientation, Type mType) {
+        this.mId = mId;
+        this.mDateTaken = mDateTaken;
+        this.mDateModified = mDateModified;
+        this.mUri = mUri;
+        this.mMimeType = mMimeType;
+        this.mOrientation = mOrientation;
+        this.mType = mType;
+    }
+
+    public static MediaStoreData getInstance(long mId, long mDateTaken, long mDateModified, Uri mUri, String mMimeType,
+                                             int mOrientation, Type mType) {
+        return new MediaStoreData(mId, mDateTaken, mDateModified, mUri, mMimeType, mOrientation, mType);
     }
 
     public long getDateTaken() {
@@ -31,6 +43,14 @@ public class MediaStoreData {
 
     public void setDateTaken(long mDateTaken) {
         this.mDateTaken = mDateTaken;
+    }
+
+    public long getDateModified() {
+        return mDateModified;
+    }
+
+    public void setDateModified(long mDateModified) {
+        this.mDateModified = mDateModified;
     }
 
     public long getId() {
@@ -55,5 +75,21 @@ public class MediaStoreData {
 
     public void setUri(Uri mUri) {
         this.mUri = mUri;
+    }
+
+    public int getOrientation() {
+        return mOrientation;
+    }
+
+    public void setOrientation(int mOrientation) {
+        this.mOrientation = mOrientation;
+    }
+
+    public Type getType() {
+        return mType;
+    }
+
+    public void setType(Type mType) {
+        this.mType = mType;
     }
 }
